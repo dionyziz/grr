d8c5c660ff3bef27451845882ea38c4cba6d1846
Dionysis Zindros <dionyziz@gmail.com>
2015-02-20 00:51:40 +0200

    Enable all tests except blacklisted and benchmarks
    Tests enabled:
    CheckLoaderTests,ArtifactKBTest,MultiShardedQueueManagerTest,ConfigActionTest,SearchTest,RegistryVFSTests,OSXDriverTests,RekallTestSuite,GeneralFlowsTest,FlowStateTest,TestCryptoTypeInfos,GRRArtifactTest,GRRFuseDatastoreOnlyTest,TestOSXFileParsing,AFF4SymlinkTest,FlowTestsBaseclass,ConditionTest,RekallVADParserTest

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,7 @@ echo 'Running tests...'
 # Failing for unknown reasons: AccessControlTest,StatsStoreDataQueryTest
 # Failing for OOM reasons: SqliteDataStoreTest
 
-EXCLUDE_TESTS=AccessControlTest,StatsStoreDataQueryTest,SqliteDataStoreTest,HTTPDataStoreCSVBenchmarks,HTTPDataStoreBenchmarks,MicroBenchmarks,TDBDataStoreBenchmarks,FakeDataStoreBenchmarks,AverageMicroBenchmarks,SqliteDataStoreBenchmarks,DataStoreCSVBenchmarks,CheckLoaderTests,TDBDataStoreCSVBenchmarks,ArtifactKBTest,MultiShardedQueueManagerTest,ConfigActionTest,SearchTest,RegistryVFSTests,OSXDriverTests,RekallTestSuite,GeneralFlowsTest,FlowStateTest,TestCryptoTypeInfos,AFF4Benchmark,GRRArtifactTest,GRRFuseDatastoreOnlyTest,TestOSXFileParsing,AFF4SymlinkTest,FlowTestsBaseclass,ConditionTest,RekallVADParserTest
+EXCLUDE_TESTS=AccessControlTest,StatsStoreDataQueryTest,SqliteDataStoreTest,HTTPDataStoreCSVBenchmarks,HTTPDataStoreBenchmarks,MicroBenchmarks,TDBDataStoreBenchmarks,FakeDataStoreBenchmarks,AverageMicroBenchmarks,SqliteDataStoreBenchmarks,DataStoreCSVBenchmarks,TDBDataStoreCSVBenchmarks,AFF4Benchmark
 PYTHONPATH=. python grr/run_tests.py --processes=1 --exclude_tests=$EXCLUDE_TESTS 2>&1|grep -v 'DEBUG:'|grep -v 'INFO:'
 TEST_STATUS=${PIPESTATUS[0]}
 exit $TEST_STATUS
