d08fb061349ba99a1017170917467bfefc874351
Dionysis Zindros <dionyziz@gmail.com>
2015-02-20 12:50:56 +0200

    Try re-enabling OOM test

--- run_tests.sh
+++ run_tests.sh
@@ -4,9 +4,8 @@ echo 'Running tests...'
 
 # Have to remain disabled for now:
 # Failing for unknown reasons: AccessControlTest,StatsStoreDataQueryTest
-# Failing for OOM reasons: SqliteDataStoreTest
 
-EXCLUDE_TESTS=AccessControlTest,StatsStoreDataQueryTest,SqliteDataStoreTest,HTTPDataStoreCSVBenchmarks,HTTPDataStoreBenchmarks,MicroBenchmarks,TDBDataStoreBenchmarks,FakeDataStoreBenchmarks,AverageMicroBenchmarks,SqliteDataStoreBenchmarks,DataStoreCSVBenchmarks,TDBDataStoreCSVBenchmarks,AFF4Benchmark
+EXCLUDE_TESTS=AccessControlTest,StatsStoreDataQueryTest,HTTPDataStoreCSVBenchmarks,HTTPDataStoreBenchmarks,MicroBenchmarks,TDBDataStoreBenchmarks,FakeDataStoreBenchmarks,AverageMicroBenchmarks,SqliteDataStoreBenchmarks,DataStoreCSVBenchmarks,TDBDataStoreCSVBenchmarks,AFF4Benchmark
 PYTHONPATH=. python grr/run_tests.py --processes=1 --exclude_tests=$EXCLUDE_TESTS 2>&1|grep -v 'DEBUG:'|grep -v 'INFO:'
 TEST_STATUS=${PIPESTATUS[0]}
 exit $TEST_STATUS
