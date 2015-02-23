957ea2e930d2adb2b2f4067cfd2fa0d5dd5ffe95
Dionysis Zindros <dionyziz@gmail.com>
2015-02-23 19:53:35 +0200

    Remove comment about excluded tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,6 @@
 cd ..
 echo 'Running tests...'
 
-# Have to remain disabled for now:
-# Failing for unknown reasons: StatsStoreDataQueryTest
-
 EXCLUDE_TESTS=HTTPDataStoreCSVBenchmarks,HTTPDataStoreBenchmarks,MicroBenchmarks,TDBDataStoreBenchmarks,FakeDataStoreBenchmarks,AverageMicroBenchmarks,SqliteDataStoreBenchmarks,DataStoreCSVBenchmarks,TDBDataStoreCSVBenchmarks,AFF4Benchmark
 PYTHONPATH=. python grr/run_tests.py --processes=1 --exclude_tests=$EXCLUDE_TESTS 2>&1|grep -v 'DEBUG:'|grep -v 'INFO:'
 TEST_STATUS=${PIPESTATUS[0]}
